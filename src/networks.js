f156560ca8464ddbd13d5e15b8ec7111090e9cf8
wlc- <wwwwllllcccc@gmail.com>
2018-03-03 03:28:32 +0100

    added bip44 derivation and supported addresstypes

--- src/networks.js
+++ src/networks.js
@@ -12,12 +12,14 @@ module.exports = {
     pubKeyHash: 0x00,
     scriptHash: 0x05,
     wif: 0x80,
+    bip44Derivation: 0,
+    ticker: 'BTC',
+    qrScheme: 'bitcoin', // https://github.com/bitcoin/bips/blob/master/bip-0021.mediawiki
+    title: 'Bitcoin',
+    supportedAddressTypes: ['P2PKH', 'P2SH-P2WPKH', 'P2WPKH'],
     bridgeParameterArr: [
       ['insight', 'https://insight.bitpay.com/api'],
     ],
-    ticker: 'BTC',
-    qrScheme: 'bitcoin', // https://github.com/bitcoin/bips/blob/master/bip-0021.mediawiki
-    title: 'Bitcoin'
   },
   testnet: {
     messagePrefix: '\x18Bitcoin Signed Message:\n',
@@ -29,15 +31,18 @@ module.exports = {
     pubKeyHash: 0x6f,
     scriptHash: 0xc4,
     wif: 0xef,
+    bip44Derivation: 1,
+    ticker: 'TBTC',
+    qrScheme: 'bitcoin',
+    title: 'Bitcoin',
+    supportedAddressTypes: ['P2PKH', 'P2SH-P2WPKH', 'P2WPKH'],
     bridgeParameterArr: [
       ['insight', 'https://test-insight.bitpay.com/api'],
     ],
-    ticker: 'TBTC',
-    qrScheme: 'bitcoin',
-    title: 'Bitcoin'
   },
   litecoin: {
     messagePrefix: '\x19Litecoin Signed Message:\n',
+    bech32: 'ltc',
     bip32: {
       public: 0x019da462,
       private: 0x019d9cfe
@@ -45,12 +50,18 @@ module.exports = {
     pubKeyHash: 0x30,
     scriptHash: 0x32,
     wif: 0xb0,
+    bip44Derivation: 2,
     ticker: 'LTC',
     qrScheme: 'litecoin',
-    title: 'Litecoin'
+    title: 'Litecoin',
+    supportedAddressTypes: ['P2PKH', 'P2SH-P2WPKH', 'P2WPKH'],
+    bridgeParameterArr: [
+      ['insight', 'https://insight.litecore.io/api'],
+    ]
   },
   myriad: { // \x1B in prefix is length of prefix in hex
     messagePrefix: '\x1BMyriadcoin Signed Message:\n',
+    bech32: 'my',
     bip32: {
       public: 0x0488B21E,
       private: 0x0488ADE4
@@ -58,12 +69,14 @@ module.exports = {
     pubKeyHash: 0x32,
     scriptHash: 0x09,
     wif: 0xB2,
+    bip44Derivation: 90,
+    ticker: 'XMY',
+    qrScheme: 'myriad',
+    title: 'Myriad',
+    supportedAddressTypes: ['P2PKH'],
     bridgeParameterArr: [
       ['insight', 'https://myriadexplorer.com/api'],
       ['insight', 'http://insight-myr.cryptap.us/api']
-    ],
-    ticker: 'XMY',
-    qrScheme: 'myriad',
-    title: 'Myriad'
+    ]
   }
 }
